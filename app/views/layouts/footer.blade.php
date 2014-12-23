eb5980adfe633de58425cfb809415bb4222ba5b2
Md Imran <shellprog@gmail.com>
2014-12-23 16:22:43 +0530

    change

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -1,8 +1,5 @@
 <div class="footer clearfix">
 	<div class="pull-left">
-		&copy; 2013. Londinium Admin Template by <a href="http://themeforest.net/user/Kopyov">Eugene Kopyov</a>
-	</div>
-	<div class="pull-right icons-group">
-		<a href="#"><i class="icon-screen2"></i></a><a href="#"><i class="icon-balance"></i></a><a href="#"><i class="icon-cog3"></i></a>
+		&copy; 2015. Xenon Support System
 	</div>
 </div>

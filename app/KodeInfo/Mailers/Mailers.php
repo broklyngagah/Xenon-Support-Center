e5d15d4664f67a1533beb2189e7f149fa2fe5090
Md Imran <shellprog@gmail.com>
2014-12-29 22:34:40 +0530

    mails offf

--- app/KodeInfo/Mailers/Mailers.php
+++ app/KodeInfo/Mailers/Mailers.php
@@ -10,10 +10,11 @@ use View;
 class Mailers
 {
 
+
     public function sendTo($recipient_email , $recipient_name , $subject , $view, $data = [])
     {
 
-
+		/*
         $client = \App::make("Bogardo\Mailgun\Mailgun");
 
         if (Config::get('mailgun::use_mailgun')) {
@@ -24,7 +25,7 @@ class Mailers
             Mail::send($view, $data, function ($message) use ($subject, $recipient_email , $recipient_name) {
                 $message->to($recipient_email, $recipient_email)->subject($subject);
             });
-        }
+        }*/
 
 
     }

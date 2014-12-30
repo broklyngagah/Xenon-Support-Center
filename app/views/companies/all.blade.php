613517113eefa290066877b79f10d5e4638f3529
Md Imran <shellprog@gmail.com>
2014-12-31 00:10:16 +0530

    no message

--- app/views/companies/all.blade.php
+++ app/views/companies/all.blade.php
@@ -73,7 +73,7 @@
 </div>
 
 <div id="livechat_modal" class="modal fade" tabindex="-1" role="dialog">
-	<div class="modal-dialog">
+	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>

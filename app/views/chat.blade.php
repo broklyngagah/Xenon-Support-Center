239b7a8e6a1800494678aa6dceccb680ee6bcd6c
Md Imran <shellprog@gmail.com>
2015-01-06 13:57:51 +0530

    chat

--- app/views/chat.blade.php
+++ app/views/chat.blade.php
@@ -10,19 +10,15 @@
     {{HTML::style("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css")}}
 
 
-    <link href="http://support.dev/assets/xenon_chat/style.css" rel="stylesheet" type="text/css">
-    <script src="http://support.dev/assets/xenon_chat/script.js" type="text/javascript"></script>
-
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
     <link href="http://162.243.17.238/assets/xenon_chat/style.css" rel="stylesheet" type="text/css">
     <script src="http://162.243.17.238/assets/xenon_chat/script.js" type="text/javascript"></script>
 
     <script type="text/javascript">
-
         $(document).ready(function () {
-            $("#xenon-chat-widget").XENON_Initialize({company: 1, domain: "http://162.243.17.238"});
+            $("#xenon-chat-widget").XENON_Initialize({company: 4, domain: "http://162.243.17.238"});
         });
-
-    </script>
+    </script><div id="xenon-chat-widget"></div>
 
 <body>
 

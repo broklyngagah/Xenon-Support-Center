ab1891e54886a979909c28f7eb43585b0679dd6f
Md Imran <shellprog@gmail.com>
2015-01-27 02:23:33 +0530

    dfg

--- app/routes.php
+++ app/routes.php
@@ -19,7 +19,11 @@ Route::get('/', 'AuthController@getLogin');
 
 Route::get('/log-errors', function()
 {
-    return Log::alert(file_get_contents("php://input"));
+    $inputJSON = file_get_contents('php://input');
+    $input= json_decode( $inputJSON, TRUE ); //convert JSON into array
+
+    //print_r(json_encode($input));
+    return Log::alert($input);
 });
 
 /*
